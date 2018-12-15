a4ce64f42ccd70a1fd05309d85e1d65ade021931
Jalal azimi <jalalazimi@gmail.com>
2018-12-16 01:20:35 +0330

    init project

---
+++ app/containers/home/index.js
@@ -0,0 +1,13 @@
+// @flow
+import React, { Component } from 'react';
+import Main from '../../components/main';
+
+type Props = {};
+
+export default class Index extends Component<Props> {
+  props: Props;
+
+  render() {
+    return <Main />;
+  }
+}
