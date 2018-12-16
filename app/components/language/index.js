fb21db7832d42a7f9c64bdb904dd6e4a0e6e3654
Jalal azimi <jalalazimi@gmail.com>
2018-12-17 01:08:25 +0330

    create language dropdown

--- app/components/language/index.js
+++ app/components/language/index.js
@@ -1,19 +1,53 @@
+// @flow
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import { compose } from 'recompose';
+import { compose, withState } from 'recompose';
+import Select from 'react-select';
 import theme from '../../styles/theme';
+import Lang from './data.json';
 
 const HeaderRow = styled.header`
   height: 80px;
   background: #0b3953;
+  font-size: 14px;
 `;
 
-const Header = () => (
+const options = Object.entries(Lang).map(v => ({
+  value: v[0],
+  label: v[1].nativeName
+}));
+
+const Props = {};
+
+const Header = ({ language, updateLanguage }: Props) => (
   <ThemeProvider theme={theme}>
-    <HeaderRow className="container-fluid">lang</HeaderRow>
+    <HeaderRow className="container-fluid pt-4">
+      <div className="row">
+        <div className="col-5">
+          <Select
+            value={language}
+            onChange={updateLanguage}
+            options={options}
+          />
+        </div>
+        <div className="col-2">Change</div>
+        <div className="col-5">
+          <Select
+            value={language}
+            onChange={updateLanguage}
+            options={options}
+          />
+        </div>
+      </div>
+    </HeaderRow>
   </ThemeProvider>
 );
 
-const enhance = compose();
+const enhance = compose(
+  withState('language', 'updateLanguage', {
+    value: 'en',
+    label: 'English'
+  })
+);
 
 export default enhance(Header);
