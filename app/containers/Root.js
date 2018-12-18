8bd8326e72a8b97558ff64b201c8628727d9e062
Jalal azimi <jalalazimi@gmail.com>
2018-12-18 13:43:45 +0330

    make root container stateless

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import type { Store } from '../reducers/types';
@@ -10,15 +10,12 @@ type Props = {
   history: {}
 };
 
-export default class Root extends Component<Props> {
-  render() {
-    const { store, history } = this.props;
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
-}
+const Root = ({ store, history }: Props) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>
+);
+
+export default Root;
